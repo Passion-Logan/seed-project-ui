9a63cb2902f8f10e439acc4b308cabd0ddce4267
Passion-Logan <wangdadamailbox@163.com>
2020-06-26 22:48:41 +0800

    add

--- src/pages/system/user/index.jsx
+++ src/pages/system/user/index.jsx
@@ -1,20 +1,126 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import React, { useState, useEffect } from 'react';
-import { Spin } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import { Spin, Button, Dropdown, Menu, Divider } from 'antd';
+import { PlusOutlined, DownOutlined } from '@ant-design/icons';
+import ProTable from '@ant-design/pro-table';
 import styles from './index.less';
 
-export default () => {
+const User = () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 3000);
   }, []);
+  const actionRef = useRef();
+  const columns = [
+    {
+      title: '用户账号',
+      dataIndex: 'userName',
+    },
+    {
+      title: '昵称',
+      dataIndex: 'nickName',
+    },
+    {
+      title: '性别',
+      dataIndex: 'sex',
+      valueEnum: {
+        1: {
+          text: '男',
+        },
+        2: {
+          text: '女',
+        },
+      },
+    },
+    {
+      title: '邮箱',
+      dataIndex: 'email',
+    },
+    {
+      title: '状态',
+      dataIndex: 'enabled',
+      hideInForm: true,
+      valueEnum: {
+        0: {
+          text: '禁用',
+        },
+        1: {
+          text: '正常',
+        },
+      },
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createTime',
+    },
+    {
+      title: '创建人',
+      dataIndex: 'createBy',
+    },
+    {
+      title: '操作',
+      dataIndex: 'option',
+      valueType: 'option',
+      render: (_, record) => (
+        <>
+          <a
+            onClick={() => {
+              // handleUpdateModalVisible(true);
+              // setStepFormValues(record);
+            }}
+          >
+            配置
+          </a>
+          <Divider type="vertical" />
+          <a href="">订阅警报</a>
+        </>
+      ),
+    },
+  ];
+
   return (
     <PageHeaderWrapper
       content="这是一个用户管理页面，从这里进行开子页面发！"
       className={styles.main}
     >
+      <ProTable
+        headerTitle="查询表格"
+        // 获取表格数据
+        actionRef={actionRef}
+        rowKey="key"
+        toolBarRender={(action, { selectedRows }) => [
+          <Button type="primary">
+            <PlusOutlined /> 新建
+          </Button>,
+          selectedRows && selectedRows.length > 0 && (
+            <Dropdown
+              overlay={
+                <Menu
+                  onClick={async (e) => {
+                    if (e.key === 'remove') {
+                      // 删除节点
+                      // await
+                      action.reload();
+                    }
+                  }}
+                  selectedKeys={[]}
+                >
+                  <Menu.Item key="remove">批量删除</Menu.Item>
+                </Menu>
+              }
+            >
+              <Button>
+                批量操作 <DownOutlined />
+              </Button>
+            </Dropdown>
+          ),
+        ]}
+        columns={columns}
+        rowSelection={{}}
+      />
+
       <div
         style={{
           paddingTop: 100,
@@ -26,3 +132,5 @@ export default () => {
     </PageHeaderWrapper>
   );
 };
+
+export default User;
