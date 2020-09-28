51f3c525a7c9deb481e6fc05dcfd5db3e64a1ba2
Passion-Logan <wangdadamailbox@163.com>
2020-09-28 18:01:43 +0800

    update

--- src/pages/system/menu/index.jsx
+++ src/pages/system/menu/index.jsx
@@ -1,28 +1,144 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import React, { useState, useEffect } from 'react';
-import { Spin } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import { Button, Form, Spin } from 'antd';
 import styles from './index.less';
+import ProTable from '@ant-design/pro-table';
+import { PlusOutlined } from '@ant-design/icons';
 
-export default () => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
+const Menu = () => {
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 3000);
+  // }, []);
+
+  const FormItem = Form.Item;
+  const [form] = Form.useForm();
+  const actionRef = useRef();
+  const columns = [
+    {
+      title: '菜单名称',
+      dataIndex: 'userName',
+      hideInSearch: true,
+      rules: [
+        {
+          required: true,
+          message: '账号为必填项',
+        },
+      ],
+    },
+    {
+      title: '菜单类型',
+      dataIndex: 'sex',
+      valueEnum: {
+        1: {
+          text: '目录',
+        },
+        2: {
+          text: '菜单',
+        },
+      },
+    },
+    {
+      title: '路由名称',
+      dataIndex: 'nickName',
+    },
+    {
+      title: '路由路径',
+      dataIndex: 'password',
+      hideInSearch: true,
+      hideInTable: true,
+      rules: [
+        {
+          required: true,
+          message: '密码为必填项',
+        },
+      ],
+    },
+    {
+      title: '启用',
+      dataIndex: 'sex',
+      valueEnum: {
+        1: {
+          text: '启用',
+        },
+        2: {
+          text: '停用',
+        },
+      },
+    },
+    {
+      title: '排序',
+      dataIndex: 'email',
+      hideInSearch: true,
+    },
+    {
+      title: '是否可见菜单',
+      dataIndex: 'enabled',
+      hideInForm: true,
+      valueEnum: {
+        false: {
+          text: '禁用',
+        },
+        true: {
+          text: '开启',
+        },
+      },
+    },
+    {
+      title: '操作',
+      dataIndex: 'option',
+      valueType: 'option',
+      render: (_, record) => (
+        <>
+          <a
+            onClick={() => {
+              handleUpdateModalVisible(true);
+              setStepFormValues(record);
+            }}
+          >
+            编辑
+          </a>
+          <Divider type="vertical" />
+          <a
+            onClick={() => {
+              handleUpdatePwdVisible(true);
+              setPwdValues(record);
+            }}
+          >
+            删除
+          </a>
+        </>
+      ),
+    },
+  ];
+
   return (
-    <PageHeaderWrapper
-      content="这是一个菜单管理页面，从这里进行开子页面发！"
-      className={styles.main}
-    >
-      <div
-        style={{
-          paddingTop: 100,
-          textAlign: 'center',
-        }}
+    <PageHeaderWrapper className={styles.main}>
+      <ProTable
+        headerTitle="菜单表格"
+        actionRef={actionRef}
+        rowKey={(row) => row.id}
+        toolBarRender={(action, { selectedRowKeys, selectedRows }) => [
+          <Button type="primary" >
+            <PlusOutlined /> 新建
+          </Button>,
+          selectedRows && selectedRows.length > 0 && (
+            <Button key="removes"
+            selectedKeys={[]}
+            >
+              批量删除
+            </Button>
+          )
+        ]}
+        columns={columns}
+        rowSelection={{}}
       >
-        <Spin spinning={loading} size="large" />
-      </div>
+
+      </ProTable>
     </PageHeaderWrapper>
   );
 };
+
+export default Menu;
