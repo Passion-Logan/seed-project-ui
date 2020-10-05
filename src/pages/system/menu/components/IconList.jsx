ae9a06202f9a43b12a7b99af0b5a922ed0fffaac
Passion-Logan <wangdadamailbox@163.com>
2020-10-05 21:57:29 +0800

    完成菜单组件

--- src/pages/system/menu/components/IconList.jsx
+++ src/pages/system/menu/components/IconList.jsx
@@ -1,32 +1,24 @@
 import iconEnum from './IconEnum';
-import { Button, Card, List, Menu, Modal } from 'antd';
+import { Button, Card, Modal } from 'antd';
 import { useState } from 'react';
-import { SmileOutlined } from '@ant-design/icons';
-import Item from 'antd/lib/list/Item';
-
-const MenuItem = Menu.Item;
 
 const IconList = (props) => {
   const { cancle: handleIconVisible, visible: IconVisible } = props;
 
-  const [iconStr, setIconStr] = useState(
-    'SmileOutlined,HomeOutlined,PicLeftOutlined,SettingOutlined',
-  );
+  const iconStr1 =
+    'StepForwardOutlined,FastBackwardOutlined,FastForwardOutlined,ShrinkOutlined,ArrowsAltOutlined,DownOutlined,UpOutlined,LeftOutlined,RightOutlined,CaretUpOutlined,CaretDownOutlined,CaretLeftOutlined,CaretRightOutlined,UpCircleOutlined,DownCircleOutlined,LeftCircleOutlined,RightCircleOutlined,DoubleRightOutlined,DoubleLeftOutlined,VerticalLeftOutlined,VerticalRightOutlined,VerticalAlignTopOutlined,VerticalAlignMiddleOutlined,VerticalAlignBottomOutlined,ForwardOutlined,BackwardOutlined,RollbackOutlined,EnterOutlined,RetweetOutlined,SwapOutlined,SwapLeftOutlined,SwapRightOutlined,ArrowUpOutlined,ArrowDownOutlined,ArrowLeftOutlined,ArrowRightOutlined,PlayCircleOutlined,UpSquareOutlined,DownSquareOutlined,LeftSquareOutlined,RightSquareOutlined,LoginOutlined,LogoutOutlined,MenuFoldOutlined,MenuUnfoldOutlined,BorderBottomOutlined,BorderHorizontalOutlined,PicCenterOutlined,PicLeftOutlined,PicRightOutlined,FullscreenOutlined,FullscreenExitOutlined,';
+  const iconStr2 =
+    'QuestionOutlined,QuestionCircleOutlined,PlusOutlined,PlusCircleOutlined,PauseOutlined,PauseCircleOutlined,MinusOutlined,MinusCircleOutlined,PlusSquareOutlined,MinusSquareOutlined,InfoOutlined,InfoCircleOutlined,ExclamationOutlined,ExclamationCircleOutlined,CloseOutlined,CloseCircleOutlined,CloseSquareOutlined,CheckOutlined,CheckCircleOutlined,CheckSquareOutlined,ClockCircleOutlined,WarningOutlined,IssuesCloseOutlined,StopOutlined,';
+  const iconStr3 =
+    'EditOutlined,FormOutlined,CopyOutlined,ScissorOutlined,DeleteOutlined,SnippetsOutlined,DiffOutlined,HighlightOutlined,AlignCenterOutlined,AlignLeftOutlined,AlignRightOutlined,BgColorsOutlined,BoldOutlined,ItalicOutlined,UnderlineOutlined,StrikethroughOutlined,RedoOutlined,UndoOutlined,ZoomInOutlined,ZoomOutOutlined,FontColorsOutlined,FontSizeOutlined,LineHeightOutlined,DashOutlined,SmallDashOutlined,SortAscendingOutlined,SortDescendingOutlined,DragOutlined,OrderedListOutlined,UnorderedListOutlined,RadiusSettingOutlined,ColumnWidthOutlined,ColumnHeightOutlined,';
+  const iconStr4 =
+    'AreaChartOutlined,PieChartOutlined,BarChartOutlined,DotChartOutlined,LineChartOutlined,RadarChartOutlined,HeatMapOutlined,FallOutlined,RiseOutlined,StockOutlined,BoxPlotOutlined,FundOutlined,SlidersOutlined,';
+  const iconStr5 =
+    'AndroidOutlined,AppleOutlined,WindowsOutlined,IeOutlined,ChromeOutlined,GithubOutlined,AliwangwangOutlined,DingdingOutlined,WeiboSquareOutlined,WeiboCircleOutlined,TaobaoCircleOutlined,Html5Outlined,WeiboOutlined,TwitterOutlined,WechatOutlined,YoutubeOutlined,AlipayCircleOutlined,TaobaoOutlined,SkypeOutlined,QqOutlined,MediumWorkmarkOutlined,GitlabOutlined,MediumOutlined,LinkedinOutlined,GooglePlusOutlined,DropboxOutlined,FacebookOutlined,CodepenOutlined,CodeSandboxOutlined,AmazonOutlined,GoogleOutlined,CodepenCircleOutlined,AlipayOutlined,AntDesignOutlined,AntCloudOutlined,AliyunOutlined,ZhihuOutlined,SlackOutlined,SlackSquareOutlined,BehanceOutlined,BehanceSquareOutlined,DribbbleOutlined,DribbbleSquareOutlined,InstagramOutlined,YuqueOutlined,AlibabaOutlined,YahooOutlined,RedditOutlined,SketchOutlined,';
+  const iconStr6 =
+    'AccountBookOutlined,AimOutlined,AlertOutlined,ApartmentOutlined,ApiOutlined,AppstoreAddOutlined,AppstoreOutlined,AudioOutlined,AudioMutedOutlined,AuditOutlined,BankOutlined,BarcodeOutlined,BarsOutlined,BellOutlined,BlockOutlined,BookOutlined,BorderOutlined,BorderlessTableOutlined,BranchesOutlined,BugOutlined,BuildOutlined,BulbOutlined,CalculatorOutlined,CalendarOutlined,CameraOutlined,CarOutlined,CarryOutOutlined,CiCircleOutlined,CiOutlined,ClearOutlined,CloudDownloadOutlined,CloudOutlined,CloudServerOutlined,CloudSyncOutlined,CloudUploadOutlined,ClusterOutlined,CodeOutlined,CoffeeOutlined,CommentOutlined,CompassOutlined,CompressOutlined,ConsoleSqlOutlined,ContactsOutlined,ContainerOutlined,ControlOutlined,CopyrightOutlined,CreditCardOutlined,CrownOutlined,CustomerServiceOutlined,DashboardOutlined,DatabaseOutlined,DeleteColumnOutlined,DeleteRowOutlined,DeliveredProcedureOutlined,DeploymentUnitOutlined,DesktopOutlined,DingtalkOutlined,DisconnectOutlined,DislikeOutlined,DollarCircleOutlined,DollarOutlined,DownloadOutlined,EllipsisOutlined,EnvironmentOutlined,EuroCircleOutlined,EuroOutlined,ExceptionOutlined,ExpandAltOutlined,ExpandOutlined,ExperimentOutlined,ExportOutlined,EyeOutlined,EyeInvisibleOutlined,FieldBinaryOutlined,FieldNumberOutlined,FieldStringOutlined,FieldTimeOutlined,FileAddOutlined,FileDoneOutlined,FileExcelOutlined,FileExclamationOutlined,FileOutlined,FileGifOutlined,FileImageOutlined,FileJpgOutlined,FileMarkdownOutlined,FilePdfOutlined,FilePptOutlined,FileProtectOutlined,FileSearchOutlined,FileSyncOutlined,FileTextOutlined,FileUnknownOutlined,FileWordOutlined,FileZipOutlined,FilterOutlined,FireOutlined,FlagOutlined,FolderAddOutlined,FolderOutlined,FolderOpenOutlined,FolderViewOutlined,ForkOutlined,FormatPainterOutlined,FrownOutlined,FunctionOutlined,FundProjectionScreenOutlined,FundViewOutlined,FunnelPlotOutlined,GatewayOutlined,GifOutlined,GiftOutlined,GlobalOutlined,GoldOutlined,GroupOutlined,HddOutlined,HeartOutlined,HistoryOutlined,HomeOutlined,HourglassOutlined,IdcardOutlined,ImportOutlined,InboxOutlined,InsertRowAboveOutlined,InsertRowBelowOutlined,InsertRowLeftOutlined,InsertRowRightOutlined,InsuranceOutlined,InteractionOutlined,KeyOutlined,LaptopOutlined,LayoutOutlined,LikeOutlined,LineOutlined,LinkOutlined,Loading3QuartersOutlined,LoadingOutlined,LockOutlined,MacCommandOutlined,MailOutlined,ManOutlined,MedicineBoxOutlined,MehOutlined,MenuOutlined,MergeCellsOutlined,MessageOutlined,MobileOutlined,MoneyCollectOutlined,MonitorOutlined,MoreOutlined,NodeCollapseOutlined,NodeExpandOutlined,NodeIndexOutlined,NotificationOutlined,NumberOutlined,OneToOneOutlined,PaperClipOutlined,PartitionOutlined,PayCircleOutlined,PercentageOutlined,PhoneOutlined,PictureOutlined,PlaySquareOutlined,PoundCircleOutlined,PoundOutlined,PoweroffOutlined,PrinterOutlined,ProfileOutlined,ProjectOutlined,PropertySafetyOutlined,PullRequestOutlined,PushpinOutlined,QrcodeOutlined,ReadOutlined,ReconciliationOutlined,RedEnvelopeOutlined,ReloadOutlined,RestOutlined,RobotOutlined,RocketOutlined,RotateLeftOutlined,RotateRightOutlined,SafetyCertificateOutlined,SafetyOutlined,SaveOutlined,ScanOutlined,ScheduleOutlined,SearchOutlined,SecurityScanOutlined,SelectOutlined,SendOutlined,SettingOutlined,ShakeOutlined,ShareAltOutlined,ShopOutlined,ShoppingCartOutlined,ShoppingOutlined,SisternodeOutlined,SkinOutlined,SmileOutlined,SolutionOutlined,SoundOutlined,SplitCellsOutlined,StarOutlined,SubnodeOutlined,SwitcherOutlined,SyncOutlined,TableOutlined,TabletOutlined,TagOutlined,TagsOutlined,TeamOutlined,ThunderboltOutlined,ToTopOutlined,ToolOutlined,TrademarkCircleOutlined,TrademarkOutlined,TransactionOutlined,TranslationOutlined,TrophyOutlined,UngroupOutlined,UnlockOutlined,UploadOutlined,UsbOutlined,UserAddOutlined,UserDeleteOutlined,UserOutlined,UserSwitchOutlined,UsergroupAddOutlined,UsergroupDeleteOutlined,VerifiedOutlined,VideoCameraAddOutlined,VideoCameraOutlined,WalletOutlined,WhatsAppOutlined,WifiOutlined,WomanOutlined,';
 
-  const data = [
-    {
-      icon: 'SmileOutlined',
-    },
-    {
-      icon: 'SmileOutlined',
-    },
-    {
-      icon: 'SmileOutlined',
-    },
-    {
-      icon: 'SmileOutlined',
-    },
-  ];
+  const [iconStr, setIconStr] = useState(iconStr1);
 
   const tabList = [
     {
@@ -55,22 +47,15 @@ const IconList = (props) => {
     },
   ];
 
-  // const contentList = {
-  //   // tab1: () => {
-  //   //   iconStr.split(',').map(item => (iconEnum[item]))
-  //   // },
-  //   tab1: iconStr.split(',').map(item => (iconEnum[item])),
-  //   tab2: <p>content2</p>,
-  // };
-
   const [key, setKey] = useState('tab1');
 
   const renderFooter = () => {
     return (
       <>
         <Button onClick={() => handleIconVisible(false)}>取消</Button>
-        {/* <Button type="primary" onClick={() => handleNext()}> */}
-        <Button type="primary">完成</Button>
+        <Button type="primary" onClick={() => handleIconVisible(false)}>
+          完成
+        </Button>
       </>
     );
   };
@@ -80,7 +65,6 @@ const IconList = (props) => {
       destroyOnClose
       width={800}
       visible={IconVisible}
-      // onOk={this.handleOk}
       onCancel={() => handleIconVisible(false)}
       footer={renderFooter()}
     >
@@ -88,9 +72,31 @@ const IconList = (props) => {
         style={{ width: '100%' }}
         tabList={tabList}
         activeTabKey={key}
-        onTabChange={(key) => setKey(key)}
+        onTabChange={(key) => {
+          setKey(key);
+          if (key == 'tab1') {
+            setIconStr(iconStr1);
+          }
+          if (key == 'tab2') {
+            setIconStr(iconStr2);
+          }
+          if (key == 'tab3') {
+            setIconStr(iconStr3);
+          }
+          if (key == 'tab4') {
+            setIconStr(iconStr4);
+          }
+          if (key == 'tab5') {
+            setIconStr(iconStr5);
+          }
+          if (key == 'tab6') {
+            setIconStr(iconStr6);
+          }
+        }}
       >
-        {iconStr.split(',').map((item) => iconEnum[item])}
+        {iconStr.split(',').map((item) => (
+          <div key={item}>{iconEnum[item]}</div>
+        ))}
       </Card>
     </Modal>
   );
