d93d23d57c9b8f33efc138bf3469941c0f4f6dae
Passion-Logan <wangdadamailbox@163.com>
2020-10-21 23:02:00 +0800

    update

--- mock/menu.js
+++ mock/menu.js
@@ -1,84 +1,84 @@
-export default {
-  '/api/auth/user/nav': {
-    menuData: [
-      {
-        path: '/',
-        icon: 'SmileOutlined',
-        redirect: '/welcome',
-      },
-      {
-        path: '/user',
-        icon: 'SmileOutlined',
-        children: [
-          {
-            name: 'login',
-            path: '/user/login',
-          },
-        ],
-      },
-      {
-        path: '/welcome',
-        hideInMenu: true,
-        icon: 'SmileOutlined',
-        name: 'welcome',
-      },
-      {
-        path: '/admin',
-        icon: 'SmileOutlined',
-        name: 'admin',
-        children: [
-          {
-            path: '/admin/sub-page',
-            icon: 'SmileOutlined',
-            name: 'sub-page',
-          },
-        ],
-      },
-      {
-        name: 'list.table-list',
-        icon: 'SmileOutlined',
-        path: '/list',
-      },
-      {
-        path: 'http://192.168.3.153:8080/qsyun/doc.html',
-        target: '_blank',
-        name: '文档1',
-      },
-      {
-        path: '/test',
-        icon: 'smSmileOutlinedile',
-        name: 'test',
-        children: [
-          {
-            path: '/test/sub-test',
-            icon: 'SmileOutlined',
-            name: 'sub-test',
-          },
-        ],
-      },
-      {
-        name: 'system',
-        icon: 'SettingOutlined',
-        path: '/system',
-        children: [
-          {
-            path: '/system/user',
-            name: 'system-user',
-          },
-          {
-            path: '/system/role',
-            name: 'system-role',
-          },
-          {
-            path: '/system/menu',
-            name: 'system-menu',
-          },
-          {
-            path: '/system/globle',
-            name: 'system-globle',
-          },
-        ],
-      },
-    ],
-  },
-};
+// export default {
+//   '/api/auth/user/nav': {
+//     menuData: [
+//       {
+//         path: '/',
+//         icon: 'SmileOutlined',
+//         redirect: '/welcome',
+//       },
+//       {
+//         path: '/user',
+//         icon: 'SmileOutlined',
+//         children: [
+//           {
+//             name: 'login',
+//             path: '/user/login',
+//           },
+//         ],
+//       },
+//       {
+//         path: '/welcome',
+//         hideInMenu: true,
+//         icon: 'SmileOutlined',
+//         name: 'welcome',
+//       },
+//       {
+//         path: '/admin',
+//         icon: 'SmileOutlined',
+//         name: 'admin',
+//         children: [
+//           {
+//             path: '/admin/sub-page',
+//             icon: 'SmileOutlined',
+//             name: 'sub-page',
+//           },
+//         ],
+//       },
+//       {
+//         name: 'list.table-list',
+//         icon: 'SmileOutlined',
+//         path: '/list',
+//       },
+//       {
+//         path: 'http://192.168.3.153:8080/qsyun/doc.html',
+//         target: '_blank',
+//         name: '文档1',
+//       },
+//       {
+//         path: '/test',
+//         icon: 'smSmileOutlinedile',
+//         name: 'test',
+//         children: [
+//           {
+//             path: '/test/sub-test',
+//             icon: 'SmileOutlined',
+//             name: 'sub-test',
+//           },
+//         ],
+//       },
+//       {
+//         name: 'system',
+//         icon: 'SettingOutlined',
+//         path: '/system',
+//         children: [
+//           {
+//             path: '/system/user',
+//             name: 'system-user',
+//           },
+//           {
+//             path: '/system/role',
+//             name: 'system-role',
+//           },
+//           {
+//             path: '/system/menu',
+//             name: 'system-menu',
+//           },
+//           {
+//             path: '/system/globle',
+//             name: 'system-globle',
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// };
